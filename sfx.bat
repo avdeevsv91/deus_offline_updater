2ff30be97070906b644f2cd2b2fb5d760d5d2621
Sergey Avdeev <thesoultaker48@gmail.com>
2017-03-27 16:31:16 +0300

    Added change sfx icon and image

--- sfx.bat
+++ sfx.bat
@@ -1,4 +1,5 @@
 @echo off
 
-del /q "%sfx_name%.exe"
-"C:\Program Files\WinRAR\rar.exe" a -sfxdefault.sfx -zsfx.opt sfx @sfx.lst
+del /q "sfx.exe"
+"%ProgramFiles%\WinRAR\WinRAR.exe" a -ibck -iadm -inul -sfx -iiconsfx.ico -iimgsfx.bmp -zsfx.opt sfx @sfx.lst
+if errorlevel 1 echo There was an error on creating the SFX archive!
